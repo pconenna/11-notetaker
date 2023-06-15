a5e1340194a3ad311f5330702d42cca39365fa85
Peter Conenna <pconenna@depaul.edu>
2023-06-15 13:31:44 -0500

    fixed html routes

--- routes/notes.js
+++ routes/notes.js
@@ -1,1 +1,21 @@
 const n = require('express').Router();
+const { readFromFile, writeToFile, readAndAppend } = require('../helpers/fsUtils');
+
+// get for notes
+n.get('/',(req,res) =>
+    readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+);
+
+n.post('/',(req,res) => {
+    const {title,text} = req.body;
+    if(title && text){
+        const newNote ={
+            title,
+            text
+        }
+        readAndAppend(newNote,'./db/db.json');
+    }else{
+        res.json("Error")
+    }
+})
+module.exports = n;
--- server.js
+++ server.js
@@ -8,13 +8,19 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
+
+
 // get method for index route
-app.get('/*',(req,res) =>
+app.get('/',(req,res) =>
 res.sendFile(path.join(__dirname, './public/index.html')));
 
 // get emthod for /notes
 app.get('/notes',(req,res) =>
 res.sendFile(path.join(__dirname, './public/notes.html')));
+
+// the wildcard has to be last 
+app.get('/*',(req,res) =>
+res.sendFile(path.join(__dirname, './public/index.html')));
 
 app.listen(PORT, () =>
   console.log(`App listening at http://localhost:${PORT} 🚀`)

045598e46b748eb8d543d4f2e91aa6b605ff1878
Peter Conenna <pconenna@depaul.edu>
2023-06-21 12:56:32 -0500

    changed app.listen

--- server.js
+++ server.js
@@ -23,6 +23,4 @@ res.sendFile(path.join(__dirname, './public/notes.html')));
 app.get('/*',(req,res) =>
 res.sendFile(path.join(__dirname, './public/index.html')));
 
-app.listen(PORT, () =>
-  console.log(`App listening at http://localhost:${PORT} 🚀`)
-);
+app.listen(PORT, () => console.log(`App listening on port ${PORT}`));
